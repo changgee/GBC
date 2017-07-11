4c2d42a112c7c86dc5eed7b1a8bf7e04dd87b6a5
changgee <changgee@gmail.com>
2017-07-10 23:47:33 -0400

    SimData.R

--- Sim/SimData.R
+++ Sim/SimData.R
@@ -79,17 +79,17 @@ llk <- function(X,mu,type,param)
   p = nrow(X)
   n = ncol(X)
   
-  dev = 0
+  l = 0
   for ( j in 1:p )
   {
     if ( type[j] == 0 )
-      dev = dev - n*log(sum((X[j,]-mu[j,])^2)/n)/2
+      l = l - n*log(mean((X[j,]-mu[j,])^2))/2
     else if ( type[j] == 1 )
-      dev = dev + sum(dbinom(X[j,],param[j],1/(1+exp(-mu[j,])),TRUE))
+      l = l + sum(dbinom(X[j,],param[j],1/(1+exp(-mu[j,])),TRUE))
     else if ( type[j] == 2 )
-      dev = dev + sum(dnbinom(X[j,],param[j],1/(1+exp(mu[j,])),log=TRUE))
+      l = l + sum(dnbinom(X[j,],param[j],1/(1+exp(mu[j,])),log=TRUE))
     else if ( type[j] == 3 )
-      dev = dev + sum(dpois(X[j,],exp(mu[j,]),TRUE))
+      l = l + sum(dpois(X[j,],exp(mu[j,]),TRUE))
   }
-  dev
+  l
 }
